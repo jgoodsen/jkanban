2fca5799c79a795700e2d3316ccd0bca69caa5de
jgoodsen <jgoodsen@radsoft.com>
2010-09-10 11:42:09 -0400

    expand/collapse of a blank card is now working!

--- public/javascripts/KanbanCard.js
+++ public/javascripts/KanbanCard.js
@@ -7,8 +7,9 @@ $.fn.AddKanbanCard = function(options) {
 
     var self = $(this);
 
-    var header = $('<div class="kanban_card_header"/>');
+    var html = $('<div class="kanban_card"/>');
 
+    var header = $('<div class="kanban_card_header"/>');
     var gravatars = $('<div class="kanban_card_gravatars"/>');
     $.each(opts.card.owners, function(i, owner_id) {
         var owner = User.findById(owner_id, opts.users);
@@ -18,21 +19,27 @@ $.fn.AddKanbanCard = function(options) {
         gravatars.append('<div class="gravatar unknown_gravatar">' + '...' + '</div>');
     }
     header.append(gravatars);
-
-
     header.append('<div class="expand_collapse_arrow expand">Expand/Collapse</div>');
     header.append('<div class="kanban_card_title">' + opts.card.title + '</div>');
-    var html = $('<div class="kanban_card"/>');
+
 
     html.append(header);
 
-    header.find(".expand_collapse_arrow").click(function() {
-       var self = $(this);
-       self.toggleClass("expand");
-       self.toggleClass("collapse");
-    });
+    var body = $('<div class="kanban_card_body"/>').css("display", "none");
+
+    html.append(body);
 
     self.append(html);
+
+    header.find(".expand_collapse_arrow").click(function() {
+        var arrowToggle = $(this);
+        if (arrowToggle.hasClass("expand")) {
+            $(this).parents(".kanban_card").find(".kanban_card_body").show();
+        } else {
+            $(this).parents(".kanban_card").find(".kanban_card_body").hide();
+        }
+        $(this).toggleClass("expand").toggleClass("collapse");
+    });
 
     function fullName(user) {
         return user.firstName + " " + user.lastName;
@@ -43,7 +50,7 @@ $.fn.AddKanbanCard = function(options) {
 function get_gravatar(user, size) {
     var size = size || 80;
     var randomNumber = Math.floor(Math.random() * 1000); // random url the browser from caching
-    return 'http://www.gravatar.com/avatar/' + user.gravatarHash + '.jpg?s=' + size + '&' +randomNumber;
+    return 'http://www.gravatar.com/avatar/' + user.gravatarHash + '.jpg?s=' + size + '&' + randomNumber;
 }
 
 
