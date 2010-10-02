a2c2adfb15666ac75db7d5467f67f16b4c9fd90d
@jgoodsen <jgoodsen@radsoft.com>
2010-10-02 14:10:01 -0400

    checkpoint

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jkanban::Application.config.secret_token = '3d86cad416e544624a67a851332e097d45923601b541cec09e36cd0e29bbcb5c8e79e142b6068cc0eadad4b0f80ba79943b1f852bc3ceb9c06778b0a196f7705'
